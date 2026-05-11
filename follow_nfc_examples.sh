4b35d4d7f57354f6cc047adaa2131f1329def19f
GOB <goblin52@gmail.com>
2026-05-11 15:40:54 +0900

    Fixes follow_nfc_examples.sh to fetch both NFCA and NFCB

--- follow_nfc_examples.sh
+++ follow_nfc_examples.sh
@@ -4,23 +4,26 @@ REMOTE_NAME="nfc_examples"
 REMOTE_URL="git@github.com:m5stack/M5Unit-NFC.git"
 #REMOTE_BRANCH="main"
 REMOTE_BRANCH="develop"
-SRC_DIR="examples/UnitUnified/NFCA"  # Path(M5Unit-NFC)
-DEST_DIR="examples/UnitUnified/NFCA" # Path(M5Unit-RFID)
+
+SUBDIRS=("NFCA" "NFCB")
 
 echo "--- 1. Fetch nfc_examples ---"
 git fetch $REMOTE_NAME
 
-echo "--- 2. Extracting ---"
-SHA=$(git subtree split --prefix=$SRC_DIR $REMOTE_NAME/$REMOTE_BRANCH)
-if [ -z "$SHA" ]; then
-    echo "Error: Failed to extract"
-    exit 1
-fi
+for SUBDIR in "${SUBDIRS[@]}"; do
+    SRC_DIR="examples/UnitUnified/$SUBDIR"   # Path(M5Unit-NFC)
+    DEST_DIR="examples/UnitUnified/$SUBDIR"  # Path(M5Unit-RFID)
 
-echo "Extracted SHA: $SHA"
+    echo "--- 2. Extracting $SRC_DIR ---"
+    SHA=$(git subtree split --prefix=$SRC_DIR $REMOTE_NAME/$REMOTE_BRANCH)
+    if [ -z "$SHA" ]; then
+        echo "Error: Failed to extract $SUBDIR"
+        exit 1
+    fi
+    echo "Extracted SHA: $SHA"
 
-echo "--- 3. Merge ---"
-git subtree pull --prefix=$DEST_DIR $REMOTE_NAME $SHA --squash
+    echo "--- 3. Merge into $DEST_DIR ---"
+    git subtree pull --prefix=$DEST_DIR $REMOTE_NAME $SHA --squash
+done
 
 echo "--- Done ---"
-
