b781bcfebaa99f8d68384c14dcad314f42df529d
GOB <goblin52@gmail.com>
2026-06-09 12:54:44 +0900

    Rewrite doxy.sh with POSIX grep

--- docs/doxy.sh
+++ docs/doxy.sh
@@ -2,10 +2,10 @@
 
 # Please execute on repository root
 
-## Get version from library.properties
-## Get git rev of HEAD
-LIB_VERSION="$(pcregrep -o1 "^\s*version\s*=\s*(\*|\d+(\.\d+){0,3}(\.\*)?)" library.properties)"
-#echo ${DOXYGEN_PROJECT_NUMBER}
-DOXYGEN_PROJECT_NUMBER="${LIB_VERSION} git rev:$(git rev-parse --short HEAD)" doxygen docs/Doxyfile
+## Get version from library.properties (POSIX-only: grep + cut + tr)
+LIB_VERSION="$(grep '^version' library.properties | cut -d= -f2 | tr -d ' ')"
 
+## Get git rev of HEAD (suppress suffix if not in a git repo)
+GIT_REV="$(git rev-parse --short HEAD 2>/dev/null)"
 
+DOXYGEN_PROJECT_NUMBER="${LIB_VERSION}${GIT_REV:+ git rev:$GIT_REV}" doxygen docs/Doxyfile
